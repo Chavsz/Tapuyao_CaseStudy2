6e12f469925539334a367f727768d210b0ca44db
Chavy <martinchavy383@gmail.com>
2025-03-22 23:21:16 +0800

    edit style records page

--- frontend/src/Pages/Records.jsx
+++ frontend/src/Pages/Records.jsx
@@ -146,23 +146,23 @@ function Records() {
       <Modal isOpen={isOpen} onClose={onClose}>
         <div className='resident-info'>
           <h2>Resident Information</h2>
-          <p><strong>ID:</strong> {resident.id}</p>
-          <p><strong>First Name:</strong> {resident.firstName}</p>
-          <p><strong>Last Name:</strong> {resident.lastName}</p>
-          <p><strong>Middle Name:</strong> {resident.middleName}</p>
-          <p><strong>Birth Date:</strong> {resident.birthDate}</p>
-          <p><strong>Age:</strong> {resident.age}</p>
-          <p><strong>Place of Birth:</strong> {resident.placeBirth}</p>
-          <p><strong>Address:</strong> {resident.address}</p>
-          <p><strong>Gender:</strong> {resident.gender}</p>
-          <p><strong>Voter Status:</strong> {resident.voterStatus}</p>
-          <p><strong>Civil Status:</strong> {resident.civilStatus}</p>
-          <p><strong>Email:</strong> {resident.email}</p>
-          <p><strong>Phone Number:</strong> {resident.phoneNumber}</p>
-          <p><strong>Father's Last Name:</strong> {resident.fatherLname}</p>
-          <p><strong>Father's First Name:</strong> {resident.fatherFname}</p>
-          <p><strong>Mother's Last Name:</strong> {resident.motherLname}</p>
-          <p><strong>Mother's First Name:</strong> {resident.motherFname}</p>
+          <p>ID: {resident.id}</p>
+          <p>First Name: {resident.firstName}</p>
+          <p>Last Name: {resident.lastName}</p>
+          <p>Middle Name: {resident.middleName}</p>
+          <p>Birth Date: {resident.birthDate}</p>
+          <p>Age: {resident.age}</p>
+          <p>Place of Birth: {resident.placeBirth}</p>
+          <p>Address: {resident.address}</p>
+          <p>Gender: {resident.gender}</p>
+          <p>Voter Status: {resident.voterStatus}</p>
+          <p>Civil Status: {resident.civilStatus}</p>
+          <p>Email: {resident.email}</p>
+          <p>Phone Number: {resident.phoneNumber}</p>
+          <p>Father's Last Name: {resident.fatherLname}</p>
+          <p>Father's First Name: {resident.fatherFname}</p>
+          <p>Mother's Last Name: {resident.motherLname}</p>
+          <p>Mother's First Name: {resident.motherFname}</p>
         </div>
       </Modal>
     );
@@ -172,161 +172,83 @@ function Records() {
     <div className='records' >
 
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-      <div className='form'>
-      {!isEditing ? (
-        <form onSubmit={handleAddSubmit}>
-          <div className='id-form'>
-            <div className='input-field'>
-              <label>ID</label>
-              <input type="text" name="id" placeholder="ID" value={formData.id} onChange={handleChange} required />
+        <div className='form'>
+          <form onSubmit={handleAddSubmit}>
+            <div className='id-form'>
+              <div className='input-field'>
+                <label>ID</label>
+                <input type="text" name="id" placeholder="ID" value={formData.id} onChange={handleChange} required />
+              </div>
+
+              <div className='input-field'>
+                <label>Firstname</label>
+                <input type="text" name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange}required  />
+              </div>
+
+              <div className='input-field'>
+                <label>Lastname</label>
+                <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange}required  />
+              </div>
+
+              <div className='input-field'>
+                <label>Middlename</label>
+                <input type="text" name="middleName" placeholder="Middle Name" value={formData.middleName} onChange={handleChange} required />
+              </div>
             </div>
 
-            <div className='input-field'>
-              <label>Firstname</label>
-              <input type="text" name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange}required  />
-            </div>
+            <div className='second-form'>
+              <div className='input-field'>
+                <label>Birth Date</label>
+                <input type="date" name="birthDate" placeholder="Birth Date" value={formData.birthDate} onChange={handleChange}  required />
+              </div>
+              
+              <div className='input-field'>
+                <label>Age</label>
+                <input type="number" name="age" placeholder="Age" value={formData.age} onChange={handleChange}required   />
+              </div>
 
-            <div className='input-field'>
-              <label>Lastname</label>
-              <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange}required  />
+              <div className='input-field'>
+                <label>Email</label>
+                <input type="text" name="email" placeholder="Email" value={formData.email} onChange={handleChange}required />
+              </div>
             </div>
+            <label>Place of Birth</label>
+            <input type="text" name="placeBirth" placeholder="Place of Birth" value={formData.placeBirth} onChange={handleChange} required  />
+            <label>Address</label>
+            <input type="text" name="address" placeholder="Address" value={formData.address} onChange={handleChange} required />
 
-            <div className='input-field'>
-              <label>Middlename</label>
-              <input type="text" name="middleName" placeholder="Middle Name" value={formData.middleName} onChange={handleChange} required />
-            </div>
-          </div>
+            <label>Gender</label>
+            <select name='gender' value={formData.gender} onChange={handleChange} required>
+                  <option value="">Select Gender</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+            </select>
 
-          <div className='second-form'>
-            <div className='input-field'>
-              <label>Birth Date</label>
-              <input type="date" name="birthDate" placeholder="Birth Date" value={formData.birthDate} onChange={handleChange}  required />
-            </div>
+            <label>Voter Status</label>
+            <select name='voterStatus' value={formData.voterStatus} onChange={handleChange} required>
+                  <option value="">Select</option>
+                  <option value="Registered">Registered</option>
+                  <option value="Not Registered">Not Registered</option>
+            </select>
+
+            <label>Civil Status</label>
+            <input type="text" name="civilStatus" placeholder="Civil Status" value={formData.civilStatus} onChange={handleChange} required />
             
-            <div className='input-field'>
-              <label>Age</label>
-              <input type="number" name="age" placeholder="Age" value={formData.age} onChange={handleChange}required   />
-            </div>
-
-            <div className='input-field'>
-              <label>Email</label>
-              <input type="text" name="email" placeholder="Email" value={formData.email} onChange={handleChange}required />
-            </div>
-          </div>
-          <label>Place of Birth</label>
-          <input type="text" name="placeBirth" placeholder="Place of Birth" value={formData.placeBirth} onChange={handleChange} required  />
-          <label>Address</label>
-          <input type="text" name="address" placeholder="Address" value={formData.address} onChange={handleChange} required />
-
-          <label>Gender</label>
-          <select name='gender' value={formData.gender} onChange={handleChange} required>
-                <option value="">Select Gender</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-          </select>
-
-          <label>Voter Status</label>
-          <select name='voterStatus' value={formData.voterStatus} onChange={handleChange} required>
-                <option value="">Select</option>
-                <option value="Registered">Registered</option>
-                <option value="Not Registered">Not Registered</option>
-          </select>
-
-          <label>Civil Status</label>
-          <input type="text" name="civilStatus" placeholder="Civil Status" value={formData.civilStatus} onChange={handleChange} required />
-          
-          <label>Phone Number</label>
-          <input type="number" name="phoneNumber" placeholder="Phone Number" value={formData.phoneNumber} onChange={handleChange}required  />
-          <p>Father</p>
-          <label>Last Name</label>
-          <input type="text" name="fatherLname" placeholder="Father Last Name" value={formData.fatherLname} onChange={handleChange} required  />
-          <label>First Name</label>
-          <input type="text" name="fatherFname" placeholder="Father First Name" value={formData.fatherFname} onChange={handleChange} required  />
-          <p>Mother</p>
-          <label>Last Name</label>
-          <input type="text" name="motherLname" placeholder="Mother Last Name" value={formData.motherLname} onChange={handleChange} required />
-          <label>First Name</label>
-          <input type="text" name="motherFname" placeholder="Mother First Name" value={formData.motherFname} onChange={handleChange} required />
-          <button type="submit">Add resident</button>
-        </form>
-      ) : (
-        <form onSubmit={handleEditSubmit}>
-          <div className='id-form'>
-            <div className='input-field'>
-              <label>ID</label>
-              <input type="text" name="id" placeholder="ID" value={formData.id} onChange={handleChange} required />
-            </div>
-
-            <div className='input-field'>
-              <label>Firstname</label>
-              <input type="text" name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange}required  />
-            </div>
-
-            <div className='input-field'>
-              <label>Lastname</label>
-              <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange}required  />
-            </div>
-
-            <div className='input-field'>
-              <label>Middlename</label>
-              <input type="text" name="middleName" placeholder="Middle Name" value={formData.middleName} onChange={handleChange} required />
-            </div>
-          </div>
-
-          <div className='second-form'>
-            <div className='input-field'>
-              <label>Birth Date</label>
-              <input type="date" name="birthDate" placeholder="Birth Date" value={formData.birthDate} onChange={handleChange}  required />
-            </div>
-            
-            <div className='input-field'>
-              <label>Age</label>
-              <input type="number" name="age" placeholder="Age" value={formData.age} onChange={handleChange}required   />
-            </div>
-
-            <div className='input-field'>
-              <label>Email</label>
-              <input type="text" name="email" placeholder="Email" value={formData.email} onChange={handleChange}required />
-            </div>
-          </div>
-          <label>Place of Birth</label>
-          <input type="text" name="placeBirth" placeholder="Place of Birth" value={formData.placeBirth} onChange={handleChange} required  />
-          <label>Address</label>
-          <input type="text" name="address" placeholder="Address" value={formData.address} onChange={handleChange} required />
-
-          <label>Gender</label>
-          <select name='gender' value={formData.gender} onChange={handleChange} required>
-                <option value="">Select Gender</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-          </select>
-
-          <label>Voter Status</label>
-          <select name='voterStatus' value={formData.voterStatus} onChange={handleChange} required>
-                <option value="">Select</option>
-                <option value="Registered">Registered</option>
-                <option value="Not Registered">Not Registered</option>
-          </select>
-
-          <label>Civil Status</label>
-          <input type="text" name="civilStatus" placeholder="Civil Status" value={formData.civilStatus} onChange={handleChange} required />
-          
-          <label>Phone Number</label>
-          <input type="number" name="phoneNumber" placeholder="Phone Number" value={formData.phoneNumber} onChange={handleChange}required  />
-          <p>Father</p>
-          <label>Last Name</label>
-          <input type="text" name="fatherLname" placeholder="Father Last Name" value={formData.fatherLname} onChange={handleChange} required  />
-          <label>First Name</label>
-          <input type="text" name="fatherFname" placeholder="Father First Name" value={formData.fatherFname} onChange={handleChange} required  />
-          <p>Mother</p>
-          <label>Last Name</label>
-          <input type="text" name="motherLname" placeholder="Mother Last Name" value={formData.motherLname} onChange={handleChange} required />
-          <label>First Name</label>
-          <input type="text" name="motherFname" placeholder="Mother First Name" value={formData.motherFname} onChange={handleChange} required />
-          <button type="submit">Update resident</button>
-        </form>
-      )}
-      </div>
+            <label>Phone Number</label>
+            <input type="number" name="phoneNumber" placeholder="Phone Number" value={formData.phoneNumber} onChange={handleChange}required  />
+            <p>Father</p>
+            <label>Last Name</label>
+            <input type="text" name="fatherLname" placeholder="Father Last Name" value={formData.fatherLname} onChange={handleChange} required  />
+            <label>First Name</label>
+            <input type="text" name="fatherFname" placeholder="Father First Name" value={formData.fatherFname} onChange={handleChange} required  />
+            <p>Mother</p>
+            <label>Last Name</label>
+            <input type="text" name="motherLname" placeholder="Mother Last Name" value={formData.motherLname} onChange={handleChange} required />
+            <label>First Name</label>
+            <input type="text" name="motherFname" placeholder="Mother First Name" value={formData.motherFname} onChange={handleChange} required />
+            <button type="submit">{isEditing ? 'Update Resident' : 'Add Resident'}</button>
+          </form>
+        </div>
       </Modal>
 
       <ResidentInfo
@@ -363,7 +285,6 @@ function Records() {
           <thead>
             <tr>
               <th>Actions</th>
-              <th>ID</th>
               <th>First Name</th>
               <th>Last Name</th>
               <th>Middle Name</th>
@@ -387,7 +308,6 @@ function Records() {
                     <button className='del' onClick={() => handleDelete(resident.id)}><mdIcons.MdDeleteOutline /></button>
                   </div>
                 </td>
-                <td>{resident.id}</td>
                 <td>{resident.firstName}</td>
                 <td>{resident.lastName}</td>
                 <td>{resident.middleName}</td>
